6094ef0710a2a829a56c77f021a2122ded311eb9
bashiralam185 <bashiralam185@gmail.com>
2023-04-01 20:57:49 +0600

    dashboard

--- DashBoard/DashboardWorking/Dashpractive.py
+++ DashBoard/DashboardWorking/Dashpractive.py
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
 from dash import Dash, dash_table, html
 from dash.dash_table.Format import Format, Scheme, Sign, Symbol
 import pandas as pd
@@ -7,11 +6,13 @@ from collections import OrderedDict
 app = Dash(__name__)
 
 df_typing_formatting = pd.DataFrame(OrderedDict([
-    ('city', ['NYC', 'Montreal', 'Los Angeles']),
-    ('max', [108, 99, 111]),
-    ('max_date', ['1926-07-22', '1975-08-01', '1939-09-20']),
-    ('min', [-15, -36, 28]),
-    ('min_date', ['1934-02-09', '1957-01-15', '1913-01-07'])
+    ('year', [2022]),
+    ('month', [2]),
+    ('day', [3]),
+    ('hour', [5]),
+    ('nowcast', [16.9]),
+    ('aqi', [70]),
+    ('raw', [46.0])
 ]))
 
 app.layout = html.Div([
@@ -19,69 +20,41 @@ app.layout = html.Div([
         id='typing_formatting',
         data=df_typing_formatting.to_dict('records'),
         columns=[{
-            'id': 'city',
-            'name': 'City',
-            'type': 'text'
+            'id': 'year',
+            'name': 'Year',
+            'type': 'numeric'
         }, {
-            'id': 'max',
-            'name': u'Max Temperature (˚F)',
+            'id': 'month',
+            'name': 'Month',
             'type': 'numeric',
-            'format': Format(
-                precision=0,
-                scheme=Scheme.fixed,
-                symbol=Symbol.yes,
-                symbol_suffix=u'˚F'
-            ),
-            # equivalent manual configuration
-            # 'format': {
-            #     'locale': {
-            #         'symbol': ['', '˚F']
-            #     },
-            #     'specifier': '$.0f'
-            # }
         }, {
-            'id': 'max_date',
-            'name': 'Max Temperature (Date)',
-            'type': 'datetime'
-        }, {
-            'id': 'min',
-            'name': u'Min Temperature (˚F)',
+            'id': 'day',
+            'name': 'Day',
             'type': 'numeric',
-            'format': Format(
-                nully='N/A',
-                precision=0,
-                scheme=Scheme.fixed,
-                sign=Sign.parantheses,
-                symbol=Symbol.yes,
-                symbol_suffix=u'˚F'
-            ),
-            # equivalent manual configuration
-            # 'format': {
-            #     'locale': {
-            #         'symbol': ['', '˚F']
-            #     },
-            #     'nully': 'N/A'
-            #     'specifier': '($.0f'
-            # }
-            'on_change': {
-                'action': 'coerce',
-                'failure': 'default'
-            },
-            'validation': {
-                'default': None
-            }
         }, {
-            'id': 'min_date',
-            'name': 'Min Temperature (Date)',
-            'type': 'datetime',
-            'on_change': {
-                'action': 'none'
-            }
-        }],
+            'id': 'hour',
+            'name': 'Hour',
+            'type': 'numeric',
+        },{
+            'id':'aqi',
+            'name':"AQI",
+            'type':'numeric',
+        },{
+            'id':'nowcast',
+            'name':"NowCast Conc.",
+            'type':'numeric',
+        },{
+            'id':'raw',
+            'name':"Raw Conc.",
+            'type':'numeric',
+        }
+        ],
         editable=True,
         style_table={'overflowX': 'scroll'}
     )
 ])
+
+print(df_typing_formatting)
 
 if __name__ == '__main__':
     app.run_server(debug=True)
